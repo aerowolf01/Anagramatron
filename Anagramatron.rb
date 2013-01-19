864ff7d603170c39a0a75036713387eab63f7d09
Cooper LeBrun <aerowolf01@gmail.com>
2013-01-18 21:25:17 -0800

    initialize for Anagram_list is done

--- Anagramatron.rb
+++ Anagramatron.rb
@@ -2,21 +2,22 @@
 
 class Anagram_list
   # Closest word is still possible
+
   def initialize(file_name = nil)
     # Shouldn't keep a file open, or open in write mode
     # TODO: First: add_file, add. Then add: error handling, list metadata.
     
-    if file_name == nil
-      list = Hash.new { |hash, key| hash[key] = [] } # Epic ruby bug was here, more in readme
-    elsif File.exists? file_name
-#      @list = File.open(file_name).read.each_line do |line|
-    else
-        raise "#{file_name} is not a valid file!"
+    @list = Hash.new { |hash, key| hash[key] = [] } # Epic ruby bug was here, more in readme
+    
+    if file_name != nil && File.exists? file_name
+      File.open(file_name).read.each_line do |line|
+        line = line.scan(/\w+/)
+        @list[line.shift] = line
     end
   end
 
   def alphagram(word)
-    # turn a word into its alpha value
+    # turn word into its alphagram
     word.scan(/[A-z]/).sort.join.downcase
   end
 
@@ -31,7 +32,7 @@ class Anagram_list
 
   def add(word)
     # for individual words
-    @list[alphagram(word)] << word# not going to 
+    @list[alphagram(word)] << word
     
   end
 
