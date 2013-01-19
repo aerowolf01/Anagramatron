f1a0519b1d88d0aa687161d0503c521bb2134582
Cooper LeBrun <aerowolf01@gmail.com>
2013-01-18 21:43:28 -0800

    removed add_words and added parse_file, changed initialize

--- Anagramatron.rb
+++ Anagramatron.rb
@@ -3,16 +3,20 @@
 class Anagram_list
   # Closest word is still possible
 
+  def parse_file(file)
+    File.open(file_name).read.each_line do |line|
+        line = line.scan(/\w+/)
+        @list[line.shift] += line
+  end
+
   def initialize(file_name = nil)
     # Shouldn't keep a file open, or open in write mode
     # TODO: First: add_file, add. Then add: error handling, list metadata.
     @file_name = file_name
     @list = Hash.new { |hash, key| hash[key] = [] } # Epic ruby bug was here, more in readme
     
-    if file_name != nil && File.exists? file_name
-      File.open(file_name).read.each_line do |line|
-        line = line.scan(/\w+/)
-        @list[line.shift] = line
+    if @file_name != nil && File.exists? @file_name
+      parse_file(@file_name)
     end
   end
 
@@ -32,13 +36,11 @@ class Anagram_list
   end
 
   def add(word)
-    # for individual words
     @list[alphagram(word)] << word
-    
   end
 
   def add_file(f_path)
-    # for converting whole files
+    # for adding files together
   end
 
   def search()
